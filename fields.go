f1b8af3439642ac720f9d27494f4a131534d328c
Paul Rosania <paul.rosania@gmail.com>
2014-07-16 00:27:42 -0700

    Date field parsing; might be overly strict

--- fields.go
+++ fields.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/paulrosania/go-charset/charset"
 )
@@ -704,7 +705,44 @@ func NewDateField() *DateField {
 	return &DateField{hf}
 }
 
-func (f *DateField) Parse(value string) {
+// Layouts suitable for passing to time.Parse.
+// These are tried in order.
+var dateLayouts []string
+
+func init() {
+	// Generate layouts based on RFC 5322, section 3.3.
+
+	dows := [...]string{"", "Mon, "}   // day-of-week
+	days := [...]string{"2", "02"}     // day = 1*2DIGIT
+	years := [...]string{"2006", "06"} // year = 4*DIGIT / 2*DIGIT
+	seconds := [...]string{":05", ""}  // second
+	// "-0700 (MST)" is not in RFC 5322, but is common.
+	zones := [...]string{"-0700", "MST", "-0700 (MST)"} // zone = (("+" / "-") 4DIGIT) / "GMT" / ...
+
+	for _, dow := range dows {
+		for _, day := range days {
+			for _, year := range years {
+				for _, second := range seconds {
+					for _, zone := range zones {
+						s := dow + day + " Jan " + year + " 15:04" + second + " " + zone
+						dateLayouts = append(dateLayouts, s)
+					}
+				}
+			}
+		}
+	}
+}
+
+// TODO: Evaluate aox implementation, might be more lenient
+func (f *DateField) Parse(s string) {
+	for _, layout := range dateLayouts {
+		t, err := time.Parse(layout, s)
+		if err == nil {
+			f.value = t.Format("Mon, 02 Jan 2006 15:04:05 -0700")
+			return
+		}
+	}
+	f.Error = errors.New("mail: header could not be parsed")
 }
 
 type MimeParameter struct {
